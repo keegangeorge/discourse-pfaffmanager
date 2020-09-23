b3f15f6ddbe4d7438daae0c21f5ce99bf55fc7bc
Jay Pfaffman <pfaffman@gmail.com>
2020-09-23 13:11:38 -0700

    housekeeping with constants

--- lib/pfaffmanager/engine.rb
+++ lib/pfaffmanager/engine.rb
@@ -1,4 +1,7 @@
 module Pfaffmanager
+  DO_API_KEY = "do_api_key".freeze
+  MG_API_KEY = "mg_api_key".freeze
+  MAXMIND_LICENSE_KEY = "maxmind_license_key".freeze
   class Engine < ::Rails::Engine
     engine_name "Pfaffmanager".freeze
     isolate_namespace Pfaffmanager
