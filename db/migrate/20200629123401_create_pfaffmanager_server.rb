b7debe4fd2e4a0c483e79455612679c6ecdf847b
Jay Pfaffman <pfaffman@gmail.com>
2020-09-23 13:11:07 -0700

    add some stuff to the server db

--- db/migrate/20200629123401_create_pfaffmanager_server.rb
+++ db/migrate/20200629123401_create_pfaffmanager_server.rb
@@ -4,13 +4,20 @@ class CreatePfaffmanagerServer < ActiveRecord::Migration[6.0]
     def change
       create_table :pfaffmanager_servers do |t|
         t.string :hostname, null: false, index: true
-        t.string :manager_status_json
-        t.string :inventory_repo
+        t.text :server_status_json
+        t.timestamp :server_status_updated_at
         t.string :ssh_key_private
         t.string :ssh_key_public
         t.string :discourse_api_key
+        t.string :do_api_key
+        t.string :mg_api_key
+        t.string :maxmind_license_key
+        t.string :discourse_url
+        t.text   :inventory
+        t.text   :discourse_env
+        t.text   :discourse_templates
+        t.text   :discourse_plugins
         t.references :user
-        t.timestamp :manager_status_updated_at
         t.timestamps
       end
     end
