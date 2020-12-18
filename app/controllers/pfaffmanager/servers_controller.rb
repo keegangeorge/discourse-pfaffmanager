221493407f512bf566fb5fa4001e4ae028b101fb
Jay Pfaffman <pfaffman@gmail.com>
2020-12-18 13:39:27 -0800

    servers controller, use rails log, not puts

--- app/controllers/pfaffmanager/servers_controller.rb
+++ app/controllers/pfaffmanager/servers_controller.rb
@@ -8,14 +8,14 @@ module Pfaffmanager
     before_action :ensure_logged_in
 
     def index
-      puts "\n\n\n\nServer controller INDEX user #{current_user.username} in the house.\n\n\n\n"
+      Rails.logger.warn "\n\n\n\nServer controller INDEX user #{current_user.username} in the house.\n\n\n\n"
       servers = ::Pfaffmanager::Server.where(user_id: current_user.id)
-      puts "-----------------> Server controller found #{servers.count} servers"
+      Rails.logger.warn "-----------------> Server controller found #{servers.count} servers"
       render_json_dump({ servers: servers })
     end
 
     def show
-      puts "\n\n\n\nServer controller SHOW for user #{current_user.username} in the house.\n\n\n\n"
+      Rails.logger.warn "\n\n\n\nServer controller SHOW for user #{current_user.username} in the house.\n\n\n\n"
 
       # TODO: Allow admin to see server of other users
       server = ::Pfaffmanager::Server.find_by(user_id: current_user.id, id: params[:id])
@@ -33,22 +33,22 @@ module Pfaffmanager
     end
 
     def create
-      puts "server controller Creating in Controller!!!!!! user_id: #{params[:server][:user_id]}"
+      Rails.logger.warn "server controller Creating in Controller!!!!!! user_id: #{params[:server][:user_id]}"
       create_groups = Group.where(name: SiteSetting.pfaffmanager_create_server_group).or(Group.where(name: SiteSetting.pfaffmanager_unlimited_server_group))
       can_create = create_groups && !Group.member_of(create_groups, current_user).empty? || current_user.admin
       if can_create
-        puts "Creating for the group" unless false
+        Rails.logger.warn "Creating for the group" unless false
         server = ::Pfaffmanager::Server.createServerForUser(params[:server][:user_id])
         group = Group.find_by_name(SiteSetting.pfaffmanager_create_server_group)
         if group
-          puts "Group found: #{group.name}"
+          Rails.logger.warn "Group found: #{group.name}"
           group.remove(current_user) unless current_user.admin
-          puts "removed." unless current_user.admin
+          Rails.logger.warn "removed." unless current_user.admin
         else
-          puts "pfaffmanager_create_server_group not configured."
+          Rails.logger.warn "pfaffmanager_create_server_group not configured."
         end
       else
-        puts "create denied"
+        Rails.logger.warn "create denied"
         server = {}
       end
       # I don't know why this next line was ever here
@@ -57,23 +57,23 @@ module Pfaffmanager
     end
 
     def set_server_status
-      puts "Set server status in the contro9ller!!!"
+      Rails.logger.warn "Set server status in the contro9ller!!!"
     end
 
     def update
-      puts "\n\nParams: #{params}\n--> qserver UPDATE controller id: #{params[:id]}\n"
-      puts "\nrequest_status: #{params[:request_status]}"
+      Rails.logger.warn "\n\nParams: #{params}\n--> qserver UPDATE controller id: #{params[:id]}\n"
+      Rails.logger.warn "\nrequest_status: #{params[:request_status]}"
       manage_group = Group.where(name: SiteSetting.pfaffmanager_server_manager_group)
       # TODO: make 6.months.ago a setting
       can_manage = !Group.member_of(manage_group, current_user).empty? || current_user.admin || server.created_at < 6.months.ago
 
       if can_manage
-        puts "you can manage"
+        Rails.logger.warn "you can manage"
         request = params[:server][:request].present? ? params[:server][:request].to_i : nil
-        puts "got the request"
+        Rails.logger.warn "got the request"
       else
         # TODO: raise an error? (Or stop on the front end and dno't worry here)
-        puts "You are not allowed to manage"
+        Rails.logger.warn "You are not allowed to manage"
       end
       # # TODO: Why not just past the fields rather than a name of a field?
       # # but it requires changing this and Ansible, so I'll leave it 2020-11-12
@@ -81,41 +81,41 @@ module Pfaffmanager
       # leaving the comment just in case because I'm bad at git
       #      field = params[:server][:field] unless params[:server].nil?
       #      value = params[:server][:value] unless params[:server].nil?
-      puts "gonna look"
+      Rails.logger.warn "gonna look"
       server = ::Pfaffmanager::Server.find_by(id: params[:id])
-      puts "Server? Got '#{server.hostname}'"
+      Rails.logger.warn "Server? Got '#{server.hostname}'"
 
-      puts "------------------> GOT REQUEST: #{request}" if request
+      Rails.logger.warn "------------------> GOT REQUEST: #{request}" if request
       if server
         data = server_params
-        puts "\nProcessing server! Data: #{data}"
-        puts "request status nil: #{data[:request_status].nil?}"
-        puts "current admin: #{current_user.admin}"
+        Rails.logger.warn "\nProcessing server! Data: #{data}"
+        Rails.logger.warn "request status nil: #{data[:request_status].nil?}"
+        Rails.logger.warn "current admin: #{current_user.admin}"
 
         if !data[:request_status].nil? && current_user.admin
           # THIS IS A REQUEST STATUS Update--ansible status update
-          puts "update..."
+          Rails.logger.warn "update..."
           # ansible updates server status via API
           server.request_status = data[:request_status]
           server.request_status_updated_at = Time.now
-          puts "\n\REQUEST STATUS UPDATE with #{data[:request_status]} at #{server.request_status_updated_at}\n\n"
+          Rails.logger.warn "\n\REQUEST STATUS UPDATE with #{data[:request_status]} at #{server.request_status_updated_at}\n\n"
           # elsif field && value && current_user.admin
-          #   puts 'got a field'
+          #   Rails.logger.warn 'got a field'
           #   # updates a single field via API
           #   server[field] = value
         elsif !request.nil?
           # a request to do an ansible task like install or rebuild
-          puts "Request exists: #{request}"
+          Rails.logger.warn "Request exists: #{request}"
            if request >= 0
              server.request = request
            else
-             puts "\n\nprocess running skip rebuild!!\n\n"
+             Rails.logger.warn "\n\nprocess running skip rebuild!!\n\n"
            end
            if request > 0
              server.request_status = "Processing"
            end
         else
-          puts "\n\nNORMAL server controller update!"
+          Rails.logger.warn "\n\nNORMAL server controller update!"
           # server.user_id = data[:user_id] if data[:user_id]
           # server.hostname = data[:hostname] if data[:hostname]
           server.discourse_api_key = data[:discourse_api_key] if data[:discourse_api_key]
@@ -131,7 +131,7 @@ module Pfaffmanager
           # don't try to start a build if one is running
         end
 
-        puts "server controller update about to save R: #{server.request}"
+        Rails.logger.warn "server controller update about to save R: #{server.request}"
         server.save
 
         if server.errors.present?
@@ -145,7 +145,7 @@ module Pfaffmanager
     end
 
     def server_params
-      puts "server_params . . . processing #{params}"
+      Rails.logger.warn "server_params . . . processing #{params}"
       params.require(:server).permit(
         :user_id,
         :hostname,
