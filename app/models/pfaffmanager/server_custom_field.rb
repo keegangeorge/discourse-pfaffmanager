e80a5d3f0dc8aa4b6672991048755a83d72c7d4f
Jay Pfaffman <pfaffman@gmail.com>
2021-01-16 07:57:53 -0800

    add server custom fields

---
+++ app/models/pfaffmanager/server_custom_field.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Pfaffmanager::ServerCustomField < ActiveRecord::Base
+  belongs_to :pfaffmanager_server
+end
---
+++ db/migrate/20210115223807_create_pfaffmanager_server_custom_field.rb
@@ -0,0 +1,11 @@
+class CreatePfaffmanagerServerCustomField < ActiveRecord::Migration[6.0]
+  def change
+    create_table :pfaffmanager_server_custom_fields do |t|
+      t.integer :server_id, null: false
+      t.string :name, limit: 256, null: false
+      t.text :value
+      t.timestamps null: false
+    end
+    add_index :pfaffmanager_server_custom_fields, [:server_id, :name]
+  end
+end
