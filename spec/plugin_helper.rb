c7c6494d5c23d5d593d6877e58c23a3c9679fadf
angus <angus@mcleod.org.au>
2021-03-05 09:58:34 +1100

    Add simplecov

--- spec/plugin_helper.rb
+++ spec/plugin_helper.rb
@@ -1,3 +1,15 @@
 # frozen_string_literal: true
 
 Dir[Rails.root.join("plugins/discourse-pfaffmanager/spec/fabricators/*.rb")].each { |f| require f }
+
+if ENV['SIMPLECOV']
+  require 'simplecov'
+
+  SimpleCov.start do
+    root "plugins/discourse-pfaffmanager"
+    track_files "plugins/discourse-pfaffmanager/**/*.rb"
+    add_filter { |src| src.filename =~ /(\/spec\/|\/db\/|plugin\.rb)/ }
+  end
+end
+
+require 'rails_helper'
